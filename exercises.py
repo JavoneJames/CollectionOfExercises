94c4863214f79866e5b272b07cdbdc70bfd21562
Jovane <jovanekjames@icloud.com>
2021-11-10 20:56:51 +0000

    implemented code that works out the unknown degrees of the two other angles

--- exercises.py
+++ exercises.py
@@ -1,8 +1,9 @@
-from math import pow, sqrt, sin
+from math import pow, sqrt, atan, degrees
 
 
 def ex1():
-    print("Exercise 1\n")
+    print("Exercise 1 - The premises of this exercise is to get the height and width of a right-angled triangle "
+          "then work out the degrees of the other two angles\n")
     running = True
     while running:
         try:  # prompt the user to input the height of the triangle - checks if it is positive
@@ -16,7 +17,15 @@ def ex1():
             print(e)
         else:  # calculate the hypotenuse of the triangle and round it to 1dp - prints result to screen
             triangle_hypotenuse = round(sqrt(pow(triangle_height, 2) + pow(triangle_width, 2)), 1)
-            print(f"The hypotenuse of the triangle  is {triangle_hypotenuse} cm\n")
+            print(f"The hypotenuse of the triangle  is {triangle_hypotenuse} cm")
+            #  divide height and width to calculate the radian
+            radian = atan(triangle_height / triangle_width)
+            #  converts radian to degrees 1dp
+            degree_of_angle_a = round(degrees(radian), 1)
+            print(f"The angle of A is : {degree_of_angle_a}\u0080")
+            #  get the degree of the other unknown angle by subtracting right angle by angle A
+            degree_of_angle_b = round(90 - degree_of_angle_a, 1)
+            print(f"The angle of B is : {degree_of_angle_b}\u0080")
 
 
 def check_if_input_is_positive(user_input, token):
