cd975996b7d4bf20a4b3f2f9547e7cf2c37bd99a
Jovane <jovanekjames@icloud.com>
2021-11-11 17:01:17 +0000

    minor refactoring

--- exercises.py
+++ exercises.py
@@ -2,7 +2,7 @@ from math import pow, sqrt, atan, degrees
 
 
 def ex1():
-    print("Exercise 1 - The premises of this exercise is to get the height and width of a right-angled triangle "
+    print("Exercise 1 - Get the height and width of a right-angled triangle "
           "then work out the degrees of the other two angles\n")
     running = True
     while running:
@@ -29,6 +29,7 @@ def ex1():
 
 
 def ex2():  # prompts user for input - then checks if the input is valid
+    print("Exercise 2 - Enter a number and display all the fibonacci sequence to that nth")
     user_input = int(input("How many numbers of the fibonacci sequence should be displayed? "))
     user_input = check_if_input_is_positive(user_input, "fibonacci")
     first_number = 0
@@ -43,6 +44,14 @@ def ex2():  # prompts user for input - then checks if the input is valid
         else: print(print_value)
     print("\n")
 
+def ex3():
+    print("Enter two integers and calculate their binomial coefficients ")
+    running = True
+    while running:
+        try:
+            pass
+        except:
+            pass
 
 def check_if_input_is_positive(user_input, token):
     #  checks if user input is a positive integer = if not prompts user to do so and then returns result
@@ -64,7 +73,7 @@ def determine_message_to_be_shown(token):
             return int(input("How many numbers of the fibonacci sequence should be displayed? "))
 
 def select_exercise():
-    list_of_exercises = [None, ex1, ex2]
+    list_of_exercises = [None, ex1, ex2, ex3]
     running = True
     while running:
         line = input("Select an exercise (0 or 'q' to quit): ")
